00a325afc99946ae5a76b9e3b19f8a8b9aa4a597
faza_elhafs <najah.hafshoh@gmail.com>
2024-01-12 09:57:28 +0700

    Challenge - showing a results component

--- App.vue
+++ App.vue
@@ -2,21 +2,26 @@
     <h1>Ninja Reaction Timer</h1>
     <button @click="start" :disabled="isPlaying">Play</button>
     <Block v-if="isPlaying" :delay="delay" @end="endGame" />
-    <p v-if="showResults">Reaction Time: {{ score }} ms</p>
+    <Result v-if="showResult" :score="score" />
 </template>
 
 <script>
+// Challenge
+//  - when the game ends, show the results component
+//  - output the score inside the results component
+
 import Block from './components/Block.vue'
+import Result from './components/Result.vue'
 
 export default {
     name: 'App',
-    components: { Block },
+    components: { Block, Result },
     data() {
         return {
             isPlaying: false,
             delay: null,
             score: null,
-            showResults: false
+            showResult: false
         }
     },
     methods: {
@@ -27,7 +32,7 @@ export default {
         endGame(reactionTime) {
             this.score = reactionTime
             this.isPlaying = false,
-            this.showResults = true
+            this.showResult = true
         }
     }
 }
