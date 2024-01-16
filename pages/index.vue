4a5f035bbd23ebd1544aca8548ccee025e0148b3
faza_elhafs <najah.hafshoh@gmail.com>
2024-01-16 09:28:05 +0700

    Membuat Layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,20 @@
 <template>
-    <div class="font-bold">
-        ini halaman index
+    <div class="min-h-screen flex">
+        <div class="bg-slate-600 min-h-screen grow grid grid-cols-10">
+            <div class="col-span-10 md:col-span-3 min-h-screen bg-orange-200">
+                <div class="md:fixed">content 1 fixed</div>
+            </div>
+            <div class="col-span-10 md:col-span-7 bg-orange-600">
+                <div class="min-h-screen bg-lime-200">section 1</div>
+                <div class="min-h-screen bg-lime-400">section 2</div>
+                <div class="min-h-screen bg-lime-600">section 3</div>
+            </div>
+        </div>
+        <div class="bg-slate-300 min-h-screen w-28 flex-none max-md:hidden">
+            <div class="fixed">ini sidebar</div>
+        </div>
+    
     </div>
-    <button class="btn btn-primary">Click</button>
 </template>
 
 <script>
