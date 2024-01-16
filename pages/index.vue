3c4cf6977e10a27e8d86e24a01acca1f116c5ca6
faza_elhafs <najah.hafshoh@gmail.com>
2024-01-16 12:03:43 +0700

    Use daisy themes

--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,51 @@
 <template>
-    <div class="min-h-screen flex">
+    <div data-theme="forest" class="min-h-screen flex text-default">
         <!-- MAIN CONTENT -->
-        <div class="bg-slate-600 min-h-screen grow grid grid-cols-10">
-            <div class="col-span-10 md:col-span-3 min-h-screen bg-orange-200">
+        <div class="min-h-screen grow grid grid-cols-10">
+            <!-- PROFILE CARD -->
+            <div class="col-span-10 md:col-span-3 min-h-screen">
                 <div class="md:fixed">content 1 fixed</div>
             </div>
-            <div class="col-span-10 md:col-span-7 bg-orange-600">
-                <div class="min-h-screen bg-lime-200">section 1</div>
-                <div class="min-h-screen bg-lime-400">section 2</div>
-                <div class="min-h-screen bg-lime-600">section 3</div>
+            <!-- SECTIONS -->
+            <div class="col-span-10 md:col-span-7">
+                <div class="min-h-screen">section 1</div>
+                <div class="min-h-screen">section 2</div>
+                <div class="min-h-screen">section 3</div>
             </div>
         </div>
 
         <!-- SIDE MENU -->
-        <div class="bg-slate-300 min-h-screen w-28 flex-none max-md:hidden">
+        <div class="min-h-screen w-28 flex-none max-md:hidden relative">
             <div class="fixed min-h-screen flex items-center">
-                <div class="border border-neutral p-4 rounded-full flex flex-col gap-4">
+                <div class="border border-neutral p-5 rounded-full flex flex-col gap-6">
                     <div class="tooltip tooltip-left" data-tip="Home">
-                        <lucideHome :size="22" class="text-accent" />
+                        <lucideHome :size="16" class="text-accent" />
                     </div>
                     <div class="tooltip tooltip-left" data-tip="About">
-                        <LucideUserRound :size="22" class="text-accent" />
+                        <LucideUserRound :size="16" class="text-accent" />
                     </div>
                     <div class="tooltip tooltip-left" data-tip="Resume">
-                        <LucideLayoutPanelLeft :size="22" class="text-accent" />
+                        <LucideLayoutPanelLeft :size="16" class="text-accent" />
                     </div>
                     <div class="tooltip tooltip-left" data-tip="Project">
-                        <LucideBriefcase :size="22" class="text-accent" />
+                        <LucideBriefcase :size="16" class="text-accent" />
                     </div>
                     <div class="tooltip tooltip-left" data-tip="Testimonial">
-                        <LucideUserRound :size="22" class="text-accent" />
+                        <LucideUserRound :size="16" class="text-accent" />
                     </div>
                     <div class="tooltip tooltip-left" data-tip="Content">
-                        <LucideMail :size="22" class="text-accent" />
+                        <LucideMail :size="16" class="text-accent" />
                     </div>
                 </div>
             </div>
 
-            <!-- SIDE MENU TOGGLE BUTTON -->
-            <div class="drawer-content fixed">
-                <!-- Page content here -->
-                <label for="my-drawer-4" class="btn btn-outline btn-circle mt-8">
-                    <lucideMenu size="22" />
-                </label> -->
-            </div>
+        </div>
+        <!-- SIDE MENU TOGGLE BUTTON -->
+        <div class="md:hidden drawer-content fixed right-8">
+            <!-- Page content here -->
+            <label for="my-drawer-4" class="btn btn-outline border-neutral btn-circle mt-8">
+                <lucideMenu size="22" class="text-accent" />
+            </label>
         </div>
     </div>
 
