10cc9c9a352c9b2cdbc2da50d305c49ec6f1caf7
faza_elhafs <najah.hafshoh@gmail.com>
2024-01-17 09:01:43 +0700

    Update Side Menu

--- pages/index.vue
+++ pages/index.vue
@@ -18,24 +18,24 @@
         <div class="min-h-screen w-28 flex-none max-md:hidden relative">
             <div class="fixed min-h-screen flex items-center">
                 <div class="border border-neutral p-5 rounded-full flex flex-col gap-6">
-                    <div class="tooltip tooltip-left" data-tip="Home">
-                        <lucideHome :size="16" class="text-accent" />
-                    </div>
-                    <div class="tooltip tooltip-left" data-tip="About">
-                        <LucideUserRound :size="16" class="text-accent" />
-                    </div>
-                    <div class="tooltip tooltip-left" data-tip="Resume">
-                        <LucideLayoutPanelLeft :size="16" class="text-accent" />
-                    </div>
-                    <div class="tooltip tooltip-left" data-tip="Project">
-                        <LucideBriefcase :size="16" class="text-accent" />
-                    </div>
-                    <div class="tooltip tooltip-left" data-tip="Testimonial">
-                        <LucideUserRound :size="16" class="text-accent" />
-                    </div>
-                    <div class="tooltip tooltip-left" data-tip="Content">
-                        <LucideMail :size="16" class="text-accent" />
-                    </div>
+                    <a href="#" class="tooltip tooltip-left" data-tip="Home">
+                        <lucideHome :size="16" class="text-secondary" />
+                    </a>
+                    <a href="#" class="tooltip tooltip-left" data-tip="About">
+                        <LucideUserRound :size="16" class="text-secondary" />
+                    </a>
+                    <a href="#" class="tooltip tooltip-left" data-tip="Resume">
+                        <LucideLayoutPanelLeft :size="16" class="text-secondary" />
+                    </a>
+                    <a href="#" class="tooltip tooltip-left" data-tip="Project">
+                        <LucideBriefcase :size="16" class="text-secondary" />
+                    </a>
+                    <a href="#" class="tooltip tooltip-left" data-tip="Testimonial">
+                        <LucideUserRound :size="16" class="text-secondary" />
+                    </a>
+                    <a href="#" class="tooltip tooltip-left" data-tip="Content">
+                        <LucideMail :size="16" class="text-secondary" />
+                    </a>
                 </div>
             </div>
 
@@ -51,14 +51,63 @@
 
     <!-- MOBILE MENU DRAWER -->
     <div class="drawer drawer-end">
-        <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+        <input id="my-drawer-4" type="checkbox" class="drawer-toggle" checked="true" />
         <div class="drawer-side overflow-hidden">
             <label for="my-drawer-4" aria-label="close sidebar" class="drawer-overlay"></label>
-            <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-                <!-- Sidebar content here -->
-                <li><a>Sidebar Item 1</a></li>
-                <li><a>Sidebar Item 2</a></li>
-            </ul>
+            <div class="menu p-14 w-80 min-h-full bg-base-200 text-base-content">
+                <div class="px-4 text-2xl">Menu</div>
+                <ul class="menu">
+                    <!-- Sidebar content here -->
+                    <li>
+                        <a href="#" class="flex gap items-start">
+                            <LucideHome :size="16" class="text-secondary" />
+                            <div>Home</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="flex gap items-start">
+                            <LucideUserRound :size="16" class="text-secondary" />
+                            <div>About</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="flex gap items-start">
+                            <LucideLayoutPanelLeft :size="16" class="text-secondary" />
+                            <div>Resume</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="flex gap items-start">
+                            <LucideBriefcase :size="16" class="text-secondary" />
+                            <div>Project</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="flex gap items-start">
+                            <LucideUserRound :size="16" class="text-secondary" />
+                            <div>Testimonial</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="flex gap items-start">
+                            <LucideMail :size="16" class="text-secondary" />
+                            <div>Content</div>
+                        </a>
+                    </li>
+                </ul>
+                <div class="px-4 py-4 text-2xl">Social</div>
+                <div class="menu flex flex-row gap-6 pl-6">
+                    <a href="#">
+                        <LucideInstagram :size="16" class="text-secondary" />
+                    </a>
+                    <a href="#">
+                        <LucideTwitter :size="16" class="text-secondary" />
+                    </a>
+                    <a href="#">
+                        <LucideGlobe2 :size="16" class="text-secondary" />
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 </template>
