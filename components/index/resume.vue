ca27f3ff9f04cac420db3b433589dd9bb1674819
faza_elhafs <najah.hafshoh@gmail.com>
2024-01-18 10:59:15 +0700

    Index Resume

--- components/index/resume.vue
+++ components/index/resume.vue
@@ -1,10 +1,31 @@
 <template>
-    <div class="min-h-screen" id="resume">
-        <div>Resume</div>
-        <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem dolorum quod culpa mollitia
-            perspiciatis veritatis corrupti maxime? Eum expedita voluptatum ex temporibus consectetur velit
-            reprehenderit aspernatur tempora est. Dolor reprehenderit assumenda fugiat voluptates quia sed repudiandae
-            tenetur
+    <div class="min-h-screen py-12" id="resume">
+        <div class="rounded-2xl border border-neutral w-fit px-4 py-1 flex items-center gap-2">
+            <LucideBriefcase :size="16" />
+            <div class="uppercase text-sm font-light">Resume</div>
+        </div>
+        <div class="flex flex-col gap-6 mt-10">
+            <div class="text-4xl font-semibold">Education & Experience</div>
+            <div>
+                <!-- list experience -->
+                <div class="flex gap-12" v-for="m in 2" :key="m">
+                    <div class="flex flex-col items-center">
+                        <LucideCircle :size="12" class="fill-neutral stroke-none" />
+                        <div class="grow w-px bg-neutral"></div>
+                    </div>
+                    <div class="mb-10">
+                        <div>2020 - Present</div>
+                        <div class="flex flex-col gap-4">
+                            <div class="text-3xl font-semibold mt-5">Framer Desinger & Developer</div>
+                            <div class="font-light">Brunodee Agency</div>
+                        </div>
+                        <div>
+                            <div class="text-3xl font-semibold">Front-End WordPress Developer</div>
+                            <div class="font-light">Envato Market</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
