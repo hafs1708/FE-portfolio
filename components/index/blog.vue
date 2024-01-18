bb2c5624dead381a1059db08db3d3a3dc609ad9d
faza_elhafs <najah.hafshoh@gmail.com>
2024-01-18 11:33:28 +0700

    Update blog

--- components/index/blog.vue
+++ components/index/blog.vue
@@ -9,12 +9,18 @@
             <!-- LIST BLOG -->
             <div class="grid grid-cols-10 gap-3" v-for="m in 4" :key="m">
                 <!-- IMAGE -->
-                <div class="aspect-video bg-neutral col-span-3 rounded-lg"></div>
-                <div class="col-span-7">
+                <div class="aspect-video bg-neutral col-span-5 lg:col-span-4 xl:col-span-3 rounded-lg"></div>
+                <div class="col-span-6 xl:col-span-7">
                     <div class="text-2xl font-semibold">Judul Blog</div>
-                    <div class="font-light">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus numquam quidem iste fuga ducimus
-                        eveniet nisi temporibus asperiores ullam. Aliquam?
+                    <div class="font-light line-clamp-2 xl:line-clamp-3">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt error consequuntur fuga voluptate
+                        corrupti maiores architecto quia doloribus est, vitae dolor eaque facere atque, voluptatem
+                        doloremque modi aliquam animi quas perspiciatis amet distinctio expedita porro numquam! Rerum maxime
+                        laborum quos illum corrupti est dignissimos magni atque incidunt ipsa ipsum id modi similique nemo,
+                        iste saepe voluptatem illo debitis? Consequuntur, quidem! Dolore laudantium, incidunt maxime, quae
+                        nesciunt, necessitatibus ullam dolores nobis sequi tenetur tempora magnam perspiciatis accusantium
+                        quas rerum soluta? Qui, quasi excepturi totam nesciunt rem delectus sapiente, sed, dicta ullam
+                        placeat quibusdam laudantium exercitationem dolorum? Voluptatum esse illo quisquam eum?
                     </div>
                 </div>
             </div>
