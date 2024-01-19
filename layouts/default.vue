6865b883458e043d2741ed5f40c0978c053220f0
faza_elhafs <najah.hafshoh@gmail.com>
2024-01-19 12:05:21 +0700

    create component side menu, menu drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,27 +5,19 @@
             <slot></slot>
         </div>
         <!-- SIDE MENU -->
-        <div class="min-h-screen w-28 flex-none max-md:hidden relative">
-            <div class="fixed min-h-screen flex items-center">
-                <div class="border border-neutral p-5 rounded-full flex flex-col gap-6">
-                    <NuxtLink v-for="(menu, i) in menus" :key="i" :to="{ path: menu.path, hash: menu.hash }"
-                        class="tooltip tooltip-left" :data-tip="menu.title">
-                        <component :is="menu.icon" :size="16" class="text-secondary" />
-                    </NuxtLink>
-                </div>
-            </div>
-        </div>
-        <!-- SIDE MENU TOGGLE BUTTON -->
-        <div class="z-10 md:hidden drawer-content fixed right-8">
-            <!-- Page content here -->
-            <label @click="toggleDrawer = !toggleDrawer" class="btn btn-outline border-neutral btn-circle mt-8">
-                <lucideMenu :size="22" class="text-accent" />
-            </label>
-        </div>
+        <DefaultLayoutSideMenu :menus="menus" />
+    </div>
+
+    <!-- SIDE MENU TOGGLE BUTTON -->
+    <div class="z-10 md:hidden drawer-content fixed right-8">
+        <!-- Page content here -->
+        <label @click="toggleDrawer = !toggleDrawer" class="btn btn-outline border-neutral btn-circle mt-8">
+            <lucideMenu :size="22" class="text-accent" />
+        </label>
     </div>
 
     <!-- MOBILE MENU DRAWER -->
-    <IndexDefaultLayoutMenuDrawer :toggleDrawer="toggleDrawer" :menus="menus" />
+    <DefaultLayoutMenuDrawer :toggleDrawer="toggleDrawer" :menus="menus" />
 </template>
 
 <script setup>
