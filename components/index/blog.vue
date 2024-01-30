eb69b3b2bc2fcc6283ee9616d4141c929f35bd54
faza_elhafs <najah.hafshoh@gmail.com>
2024-01-30 12:09:56 +0700

    handle blog with photo

--- components/index/blog.vue
+++ components/index/blog.vue
@@ -9,9 +9,17 @@
             <!-- LIST BLOG -->
             <div class="grid grid-cols-10 gap-3 group hover:bg-cyan-950 p-2 rounded-xl" v-for="(blog, i) in blogs" :key="i">
                 <!-- IMAGE -->
-                <div class="aspect-video bg-neutral col-span-4 xl:col-span-3 rounded-lg group-hover:scale-105 duration-300">
+                <div class="col-span-4 xl:col-span-3 rounded-lg">
+                    <!-- foto dummy -->
+                    <div v-if="!blog.photos.length" class="aspect-video bg-neutral group-hover:scale-105 duration-300">
+                    </div>
+
+                    <!-- foto pertama -->
+                    <img v-else :src="apiUri + blog.photos[0].path" :alt="blog.title" class="object-cover">
                 </div>
+
                 <div class="col-span-6 xl:col-span-7 flex flex-col">
+                    <!-- title -->
                     <div class="text-2xl font-semibold group-hover:text-accent">{{ blog.title }}</div>
                     <div class="grow font-light line-clamp-2 xl:line-clamp-3">{{ blog.content }}</div>
                     <div class="grow font-light">{{ blog.shortDateTime }}</div>
@@ -25,5 +33,10 @@
 const props = defineProps({
     blogs: Array
 });
+
+const config = useRuntimeConfig()
+const apiUri = config.public.apiUri;
+console.log(apiUri);
+
 console.log(props);
 </script>
