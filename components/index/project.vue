94cccb722cc74f9feef60366320229f16d316e9d
faza_elhafs <najah.hafshoh@gmail.com>
2024-01-31 10:41:35 +0700

    draw project skill

--- components/index/project.vue
+++ components/index/project.vue
@@ -11,15 +11,24 @@
                 <!-- PROJECT ITEM -->
                 <div class="col-span-2 md:col-span-1 group" v-for="(project, i) in projects" :key="i">
                     <div class="flex flex-col">
-                        <div class="font-semibold text-xl text-accent group-hover:text-accent">{{ project.title }}</div>
+                        <div class="font-semibold text-xl text-accent group-hover:text-accent truncate">{{ project.title }}
+                        </div>
                         <div>{{ project.readStartDate }} - {{ project.readEndDate }}</div>
                     </div>
+
                     <!-- IMAGE -->
-                    <div class="aspect-video bg-neutral rounded-xl group-hover:scale-110 duration-300"></div>
+                    <div class="rounded-xl aspect-video overflow-hidden group-hover:scale-105 duration-300">
+                        <!-- Tampilkan photo pertama -->
+                        <img v-if="project.photos.length" :src="apiUri + project.photos[0].path" :alt="project.title"
+                            class="object-cover">
+                        <!-- Tampilkan dummy -->
+                        <div v-else class="aspect-video bg-neutral"></div>
+                    </div>
+
                     <!-- SKILL -->
                     <div class="flex flex-nowrap overflow-hidden gap-2 mt-2">
-                        <div v-for="m in 10" :key="m" class="border border-neutral rounded-xl px-3 text-nowrap">Skill {{ m
-                        }}</div>
+                        <div v-for="skill in project.skills" :key="skill"
+                            class="border border-neutral rounded-xl px-3 text-nowrap">{{ skill.Skill.title }}</div>
                     </div>
                 </div>
             </div>
@@ -28,7 +37,15 @@
 </template>
 
 <script setup>
-defineProps({
+const props = defineProps({
     projects: Array
-})
+});
+
+console.log(props);
+
+const config = useRuntimeConfig()
+const apiUri = config.public.apiUri;
+console.log(apiUri);
+
+
 </script>
