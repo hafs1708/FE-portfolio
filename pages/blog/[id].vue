071e8eb89c9a70ff4a17d45b9865e1d17997db4e
faza_elhafs <najah.hafshoh@gmail.com>
2024-02-02 10:43:42 +0700

    draw photos with carousel

--- pages/blog/[id].vue
+++ pages/blog/[id].vue
@@ -1,17 +1,35 @@
 <template>
     <div class="max-w-7xl mx-auto">
         <!--TITLE-->
-        <div class="text-4xl font-semibold">{{ blog.title }}</div>
+        <div class="text-4xl font-semibold my-4">{{ blog.title }}</div>
+
         <!--PHOTOS-->
         <div>
-            <img v-for="photo in blog.photos" :key="photo" :src="apiUri + photo.path" alt="blog.title">
+            <Carousel :items-to-show="1.5" :wrapAround="true" :autoplay="1000">
+                <Slide v-for="photo in blog.photos" :key="photo.id">
+                    <div class="asppect-video w-full">
+                        <div class="w-full h-full flex justify-center bg-contain">
+                            <img :src="apiUri + photo.path" :alt="blog.title" class="h-full">
+                        </div>
+                    </div>
+                </Slide>
+
+                <template #addons>
+                    <Navigation />
+                    <Pagination />
+                </template>
+            </Carousel>
         </div>
+
         <!--CONTENT-->
-        <div>{{ blog.content }}</div>
+        <div class="my-4">{{ blog.content }}</div>
     </div>
 </template>
 
 <script setup>
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+
 const route = useRoute();
 const blogID = route.params.id;
 
