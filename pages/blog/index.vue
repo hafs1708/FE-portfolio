4ff5a97139c880347bf6b51e5e78d28215fb8633
faza_elhafs <najah.hafshoh@gmail.com>
2024-02-03 09:15:17 +0700

    create pagination

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,12 +2,21 @@
     <div class="max-w-7xl mx-auto px-4 pb-16">
         <!--HEADER-->
         <div class="flex justify-between items-end my-4 border-b border-b-neutral pt-6 max-md:pt-20 pb-3">
-            <div class="text-4xl font-bold">fullName</div>
-            <div>blog</div>
+            <div class="text-4xl font-bold">Fullname</div>
+            <div class="text-xl">Blogs</div>
+        </div>
+
+        <!--PAGINATION BUTTON-->
+        <div class="flex justify-between items-end">
+            <div class="text-4xl font-semibold my-6">My Latest <span class="text-accent">Blogs</span></div>
+            <div class="join">
+                <button class="join-item btn" :class="{ 'btn-disabled': page == 1 }" @click="page--">«</button>
+                <button class="join-item btn">Page {{ blogs.page }}</button>
+                <button class="join-item btn" :class="{ 'btn-disabled': !page == maxPage }" @click="page++">»</button>
+            </div>
         </div>
 
         <!--LOOPS DATA BLOGS-->
-        <div class="text-4xl font-semibold my-6">My Latest <span class="text-accent">Blogs</span></div>
         <div class="grid grid-cols-3 gap-6">
             <NuxtLink :to="'blog/' + blog.id" v-for="blog in blogs.data" :key="blog" class="w-full group">
                 <div class="text-accent text-xl font-bold">{{ blog.title }}</div>
@@ -22,6 +31,15 @@
                 </div>
             </NuxtLink>
         </div>
+
+        <!--PAGINATION BUTTON-->
+        <div class="flex justify-between items-end mt-6">
+            <div class="join">
+                <button class="join-item btn" :class="{ 'btn-disabled': page == 1 }" @click="page--">«</button>
+                <button class="join-item btn">Page {{ blogs.page }}</button>
+                <button class="join-item btn" :class="{ 'btn-disabled': !page == maxPage }" @click="page++">»</button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -30,5 +48,14 @@ const config = useRuntimeConfig()
 const apiUri = config.public.apiUri;
 
 // ambil data blogs melalui server nuxt
-const blogs = await $fetch('/api/blog');
+const blogs = ref(null);
+blogs.value = await $fetch('/api/blog');
+
+const maxPage = blogs.value.maxPage;
+const page = ref(1);
+
+watchEffect(async () => {
+    blogs.value = await $fetch('/api/blog?page=' + page.value);
+});
+
 </script>
