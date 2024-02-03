b71f076bfee86b349464cf2de1fdc33d35d409de
faza_elhafs <najah.hafshoh@gmail.com>
2024-02-03 09:52:56 +0700

    handle pagination

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -6,40 +6,43 @@
             <div class="text-xl">Blogs</div>
         </div>
 
-        <!--PAGINATION BUTTON-->
-        <div class="flex justify-between items-end">
-            <div class="text-4xl font-semibold my-6">My Latest <span class="text-accent">Blogs</span></div>
-            <div class="join">
-                <button class="join-item btn" :class="{ 'btn-disabled': page == 1 }" @click="page--">«</button>
-                <button class="join-item btn">Page {{ blogs.page }}</button>
-                <button class="join-item btn" :class="{ 'btn-disabled': !page == maxPage }" @click="page++">»</button>
-            </div>
-        </div>
-
-        <!--LOOPS DATA BLOGS-->
-        <div class="grid grid-cols-3 gap-6">
-            <NuxtLink :to="'blog/' + blog.id" v-for="blog in blogs.data" :key="blog" class="w-full group">
-                <div class="text-accent text-xl font-bold">{{ blog.title }}</div>
-                <div class="text-sm font-light">{{ blog.shortDateTime }}</div>
-                <div class="group-hover:scale-105 duration-300 mt-1">
-                    <!--image data-->
-                    <img v-if="blog.photos.length" :src="apiUri + blog.photos[0].path" alt=""
-                        class="w-full aspect-video rounded-xl">
-                    <!--image dummy-->
-                    <div v-else class="w-full aspect-video bg-neutral rounded-xl"></div>
-                    <div class="lime-clamp-2 font-light">{{ blog.content }}</div>
+        <template v-if="blogs">
+            <!--PAGINATION BUTTON-->
+            <div class="flex max-md:flex-col justify-between items-end">
+                <div class="max-md:w-full text-4xl font-semibold my-6">My Latest <span class="text-accent">Blogs</span>
                 </div>
-            </NuxtLink>
-        </div>
-
-        <!--PAGINATION BUTTON-->
-        <div class="flex justify-between items-end mt-6">
-            <div class="join">
-                <button class="join-item btn" :class="{ 'btn-disabled': page == 1 }" @click="page--">«</button>
-                <button class="join-item btn">Page {{ blogs.page }}</button>
-                <button class="join-item btn" :class="{ 'btn-disabled': !page == maxPage }" @click="page++">»</button>
+                <div class="join my-2">
+                    <button class="join-item btn" :class="{ 'btn-disabled': page == 1 }" @click="page--">«</button>
+                    <button class="join-item btn">Page {{ blogs.page }}</button>
+                    <button class="join-item btn" :class="{ 'btn-disabled': !page == maxPage }" @click="page++">»</button>
+                </div>
             </div>
-        </div>
+
+            <!--LOOPS DATA BLOGS-->
+            <div class="grid grid-cols-3 gap-6">
+                <NuxtLink :to="'blog/' + blog.id" v-for="blog in blogs.data" :key="blog" class="w-full group">
+                    <div class="text-accent text-xl font-bold">{{ blog.title }}</div>
+                    <div class="text-sm font-light">{{ blog.shortDateTime }}</div>
+                    <div class="group-hover:scale-105 duration-300 mt-1">
+                        <!--image data-->
+                        <img v-if="blog.photos.length" :src="apiUri + blog.photos[0].path" alt=""
+                            class="w-full aspect-video rounded-xl">
+                        <!--image dummy-->
+                        <div v-else class="w-full aspect-video bg-neutral rounded-xl"></div>
+                        <div class="lime-clamp-2 font-light">{{ blog.content }}</div>
+                    </div>
+                </NuxtLink>
+            </div>
+
+            <!--PAGINATION BUTTON-->
+            <div class="flex justify-end items-end my-5">
+                <div class="join my-2">
+                    <button class="join-item btn" :class="{ 'btn-disabled': page == 1 }" @click="page--">«</button>
+                    <button class="join-item btn">Page {{ blogs.page }}</button>
+                    <button class="join-item btn" :class="{ 'btn-disabled': !page == maxPage }" @click="page++">»</button>
+                </div>
+            </div>
+        </template>
     </div>
 </template>
 
@@ -49,13 +52,21 @@ const apiUri = config.public.apiUri;
 
 // ambil data blogs melalui server nuxt
 const blogs = ref(null);
-blogs.value = await $fetch('/api/blog');
-
-const maxPage = blogs.value.maxPage;
+const maxPage = ref(1);
 const page = ref(1);
 
-watchEffect(async () => {
+const fetchData = async () => {
     blogs.value = await $fetch('/api/blog?page=' + page.value);
+    maxPage.value = blogs.value.maxPage;
+}
+
+// register on before moount
+onBeforeMount(async () => {
+    await fetchData();
 });
 
+// watch effect
+watchEffect(async () => {
+    await fetchData();
+});
 </script>
